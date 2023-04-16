a156cdb871e560877a30ab6c0cecef952e78d4b3
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-04-16 22:45:45 +0900

    fix: notification service

--- src/main/kotlin/com/goofy/sse/service/NotificationService.kt
+++ src/main/kotlin/com/goofy/sse/service/NotificationService.kt
@@ -2,26 +2,27 @@ package com.goofy.sse.service
 
 import com.goofy.sse.model.NotificationEventModel
 import com.goofy.sse.service.SseEmitterEvent.Companion.send
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.net.URLEncoder
-import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import java.util.concurrent.Executors
 
 @Service
 class NotificationService {
+    private val logger = KotlinLogging.logger {}
+
     fun notifyV1(): SseEmitter {
-        // url encoding 필요
         val emitter = SseEmitterEvent.generate(180000)
+
+        // 가능하면, 코루틴으로
         val sseMvcExecutor = Executors.newSingleThreadExecutor()
 
-        println(LocalDateTime.now())
-
         sseMvcExecutor.execute {
-            try {
-                var i = 0
-                while (true) {
+            var i = 0
+            while (true) {
+                runCatching {
                     val data = NotificationEventModel(
                         id = i.toLong(),
                         message = "hello world",
@@ -35,15 +36,15 @@ class NotificationService {
                         comment = URLEncoder.encode("SSE를 활용한 이벤트 루프"),
                         data = data
                     )
-
-                    Thread.sleep(1000)
-
-                    i++
+                }.onFailure {
+                    logger.error { "error / ${it.message}" }
+                    emitter.completeWithError(it)
                 }
-            } catch (e: Exception) {
-                emitter.completeWithError(e)
+                i++
             }
         }
+
+        sseMvcExecutor.shutdown()
 
         return emitter
     }
