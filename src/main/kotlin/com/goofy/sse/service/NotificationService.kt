a589b0346c2c91f25fe67cfd5b2aeddbbb163a91
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-04-16 20:39:09 +0900

    fix: 수정

--- src/main/kotlin/com/goofy/sse/service/NotificationService.kt
+++ src/main/kotlin/com/goofy/sse/service/NotificationService.kt
@@ -1,14 +1,17 @@
 package com.goofy.sse.service
 
 import com.goofy.sse.model.NotificationEventModel
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import java.net.URLEncoder
 import java.time.ZonedDateTime
 import java.util.concurrent.Executors
 
 @Service
 class NotificationService {
     fun notifyV1(): SseEmitter {
+        // url encoding 필요
         val emitter = SseEmitter()
         val sseMvcExecutor = Executors.newSingleThreadExecutor()
 
@@ -20,13 +23,14 @@ class NotificationService {
                         .id(i.toString())
                         .name("sse event with SSE EMITTER")
                         .reconnectTime(10000)
-                        .comment("SSE를 활용한 이벤트 루프")
+                        .comment(URLEncoder.encode("SSE를 활용한 이벤트 루프"))
                         .data(
                             NotificationEventModel(
                                 id = i.toLong(),
                                 message = "hello world",
                                 createdAt = ZonedDateTime.now()
-                            )
+                            ),
+                            MediaType.APPLICATION_JSON
                         )
 
                     emitter.send(event)
